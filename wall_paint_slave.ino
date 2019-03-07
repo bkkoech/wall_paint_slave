627fc77be164666073d1d80d8a5b4222f6c99907
Billy Koech <bkkoech@college.harvard.edu>
2019-03-07 12:15:45 -0500

    added g code parser

--- wall_paint_slave.ino
+++ wall_paint_slave.ino
@@ -5,8 +5,8 @@ Pseudo code:
 Fuctions to write:
 plus_x(steps) - takes int steps returns void
 minus_x(steps) - "
-plus_z(steps) - "
-minus_z(steps) - "
+plus_y(steps) - "
+minus_y(steps) - "
 engage_sharpie() - 
 disengage_sharpie() -
 Calibrate() 
@@ -28,10 +28,10 @@ Calibrate()
 #define X_BTM_PIN_2 5
 #define X_BTM_PIN_3 6
 #define X_BTM_PIN_4 7
-#define Z_PIN_1 A5
-#define Z_PIN_2 A4
-#define Z_PIN_3 2
-#define Z_PIN_4 3
+#define Y_PIN_1 A5
+#define Y_PIN_2 A4
+#define Y_PIN_3 2
+#define Y_PIN_4 3
 #define BUTTONS_PIN A0
 #define SENSOR_PIN A1
 
@@ -39,27 +39,38 @@ Calibrate()
 // Load libraries and modules
 #include <Stepper.h>
 #include <Servo.h>
+//#include <string.h>
+
 
 //Constants and Variables
 int x_stepCount = 0; // number of steps it takes to cover the entire x length
-int z_stepCount = 0; // number of steps it takes to cover the entire z length
+int y_stepCount = 0; // number of steps it takes to cover the entire y length
 int x_pos = 0; //default x position in steps
-int y_pos = 0; //default y position in degrees
-int z_pos = 0; //default z position
+int y_pos = 0; //default y position in steps
+int z_pos = 0; //default z position in degrees
 int x_btn_left_R = 1001 ;
 int x_btn_right_R = 434;
-int z_btn_top_R = 146;
-int z_btn_bottom_R = 252;
+int y_btn_top_R = 146;
+int y_btn_bottom_R = 252;
 int R_offset = 60;
+bool DEBUG = true;
+int MODE = 5;
+
+// String data
 String receivedData = "";
+String parsed_g= "";
+String parsed_x = "";
+String parsed_y = "";
+String parsed_z = "";
+
 
 
 // Initialize stepper object for x motion
 Stepper xTopStepper(200,X_TOP_PIN_1,X_TOP_PIN_2,X_TOP_PIN_3,X_TOP_PIN_4);
 Stepper xBtmStepper(200, X_BTM_PIN_1,X_BTM_PIN_2,X_BTM_PIN_3,X_BTM_PIN_4);
 
-// Initialize stepper object for z motion
-Stepper zStepper(200,Z_PIN_1,Z_PIN_2,Z_PIN_3,Z_PIN_4);
+// Initialize stepper object for y motion
+Stepper yStepper(200,Y_PIN_1,Y_PIN_2,Y_PIN_3,Y_PIN_4);
 
 
 //x forward motion function
@@ -91,39 +102,39 @@ void move_x_reverse(int no_of_steps) {
 }
 */
 
-//z motion function
-void move_z(int no_of_steps) {
-  zStepper.step(-1*no_of_steps); // move top z stepper
+//y motion function
+void move_y(int no_of_steps) {
+  yStepper.step(-1*no_of_steps); // move top y stepper
 }
 
 
 // create servo object to control a servo
-Servo yServo; 
+Servo zServo; 
 
 // y motion function
 void set_angle(int angle){
-  yServo.write(angle);  // set servo angle
+  zServo.write(angle);  // set servo angle
 }
 
 
 // function to move all 3 axes simultaneosly
-// takes x steps to move, y angle to set servo to and z steps to move
-void move_axes(int x_steps, int z_steps, int y_angle){
+// takes x steps to move, z angle to set servo to and y steps to move
+void move_axes(int x_steps, int y_steps, int z_angle){
   int xsteps = abs(x_steps);
-  int yangle = y_angle;
-  int zsteps = abs(z_steps);
-  while(xsteps!=0 || yangle!=0 || zsteps!=0){
+  int zangle = z_angle;
+  int ysteps = abs(y_steps);
+  while(xsteps!=0 || zangle!=0 || ysteps!=0){
      if(xsteps!=0){
       move_x(x_steps/abs(x_steps));
       xsteps -= 1;
      }
-     if(yangle!=0){
-      set_angle(yangle);
-      yangle -= y_angle;
+     if(zangle!=0){
+      set_angle(zangle);
+      zangle -= z_angle;
      }
-     if(zsteps!=0){
-      move_z(z_steps/abs(z_steps));
-      zsteps -= 1;
+     if(ysteps!=0){
+      move_y(y_steps/abs(y_steps));
+      ysteps -= 1;
      }
   }
   
@@ -131,22 +142,27 @@ void move_axes(int x_steps, int z_steps, int y_angle){
 
 
 
-void go_to_coordinates(int x, int z, int y_angle){
+void go_to_coordinates(int x, int y, int z_angle){
   // find difference between current position and next position
   int x_diff = x - x_pos;
-  int z_diff = z - z_pos;
+  int y_diff = y - y_pos;
 
-  //move to new postion and set x_pos and y_pos
-  move_axes(x_diff, z_diff, y_angle);
+  
+
+  //move to new postion and set x_pos, y_pos and z_pos
+  move_axes(x_diff, y_diff, z_angle);
+  x_pos = x;
+  y_pos = y;
+  z_pos = z_angle;
 }
 
 // calibration function
 bool calibrate(){
   /* Step 1: Calibrate
    * Move hub towards -x until switch is triggered at the end 
-   * Move hub towards -z until switch is triggered at the top
+   * Move hub towards -y until switch is triggered at the top
    * Move +x while counting steps, store number of steps. No. of Steps = width.
-   * Move -z while counting steps, store number of steps. No. of Steps = height. 
+   * Move -y while counting steps, store number of steps. No. of Steps = height. 
    * Move hub to origin, x = 0, y = 0 
    */
    bool calibrated = false;
@@ -185,29 +201,29 @@ bool calibrate(){
     
    }
    while(!bottom_trig){
-    // move z to bottom until switch is trigerred
+    // move y to bottom until switch is trigerred
     sensorVal = analogRead(BUTTONS_PIN);
-    if (z_btn_bottom_R-R_offset < sensorVal && sensorVal < z_btn_bottom_R+R_offset){
+    if (y_btn_bottom_R-R_offset < sensorVal && sensorVal < y_btn_bottom_R+R_offset){
       bottom_trig = true; // stop moving
     }
     else{
-      move_z(-1);
+      move_y(-1);
     }
     
    }
 
    while(!top_trig){
-    // move z to top until switch is trigerred
+    // move y to top until switch is trigerred
     sensorVal = analogRead(BUTTONS_PIN);
-    if (z_btn_top_R-R_offset < sensorVal && sensorVal < z_btn_top_R+R_offset){
+    if (y_btn_top_R-R_offset < sensorVal && sensorVal < y_btn_top_R+R_offset){
       top_trig = true; //stop moving
-      move_z(-z_stepCount);//go back to origin
-      z_pos = 0; //reset x coordinate to 0
+      move_y(-y_stepCount);//go back to origin
+      y_pos = 0; //reset y coordinate to 0
       
     }
     else{
-      move_z(1);
-      z_stepCount +=1; // start counting steps
+      move_y(1);
+      y_stepCount +=1; // start counting steps
     }
     
    }
@@ -217,10 +233,12 @@ bool calibrate(){
 }
 
 
+
+
 // fuction to listen for commands from master(Raspberry Pi)
 void listenToPi() {
 
-  
+  // Serial.print("Listening for serial data... \n");
   // Wait for data to be available
   while (Serial.available() > 0) {
 
@@ -229,35 +247,76 @@ void listenToPi() {
     char inChar = (char)Serial.read();
     //add it to the receivedData
     receivedData += inChar;
+
     //break loop if incoming character is a new line
     if(inChar == '\n'){
-      //confirm that data was received
-      Serial.print("Received");
+      //if data received then break
       break;
-    }
+      }
   }
+
   
 
   //return value
   // return receivedData;
 }
 
- void parseSerialData(char *data){
-  // check fuction being called
-  if(data == "move_axes"){
-    // extract values of function
-    //call move_axes and invoke with arguments
+
+ void parseSerialData(String data){
+
+  // variable to keep track of comma position
+  int commas = 0;
+
+  //reset variables
+  parsed_g= "";
+  parsed_x = "";
+  parsed_y = "";
+  parsed_z = "";
+
+  //iterate over data
+  for(int i=0; i<=200; i++){
+
+    //parse data
+    if (data[i] == '\0'){ //end of data
+      break; // exit
+    }else if(data[i] != ',' && commas == 0){
+      //get g part of g code
+      parsed_g += data[i];
+    }else if(data[i] !=',' && commas == 1){
+      // get x value
+      parsed_x += data[i];
+    }else if (data[i] !=',' && commas == 2){
+      // get y value
+      parsed_y += data[i];
+    }else if (data[i] !=',' && commas == 3){
+      //get z value
+      parsed_z += data[i];
+    }
+
+    //comma incrementation
+    if(data[i] == ','){
+      commas += 1;
+    }else{
+      // do nothing
+    }
     
-  }else if(data == "go_to_coordinates"){
+  }
+  
+  if (parsed_g == "G00"){
+    Serial.print(" move ");  
+  }else if(parsed_g == "go_to_coordinates"){
     //call go to coordinates and invoke with arguments
     
-  }else if(data == "calibrate"){
+  }else if(parsed_g == "calibrate"){
     // call calibrate
     
   }else{ // run this if none match
+    Serial.print("Data input into parse: ");
     Serial.print(data);
+    Serial.print(" <-- -->");
+    Serial.print("sysError: g function does not exist");
     Serial.print("\n");
-    Serial.print("sysError: function does not exist");
+    
   }
 
   
@@ -295,16 +354,22 @@ void listenToPi() {
 void setup() {
 
   //speed
-  xTopStepper.setSpeed(60); //rpms
-  xBtmStepper.setSpeed(60); //rpms
-  zStepper.setSpeed(60); //rpms
+  xTopStepper.setSpeed(100); //rpms
+  xBtmStepper.setSpeed(100); //rpms
+  yStepper.setSpeed(100); //rpms
   
   // attaches the servo on pin 9 to the servo object
-  yServo.attach(9); 
+  zServo.attach(9); 
   set_angle(110); 
 
   // reserve 200 bytes for the receivedData:
   receivedData.reserve(200);
+
+  //reserve
+  parsed_g.reserve(5);
+  parsed_x.reserve(5);
+  parsed_y.reserve(5);
+  parsed_z.reserve(5);
   
   // initialize the serial port:
   Serial.begin(9600);
@@ -318,44 +383,120 @@ void setup() {
   pinMode(X_BTM_PIN_2, OUTPUT);
   pinMode(X_BTM_PIN_3, OUTPUT);
   pinMode(X_BTM_PIN_4, OUTPUT);
-  pinMode(Z_PIN_1, OUTPUT);
-  pinMode(Z_PIN_2, OUTPUT);
-  pinMode(Z_PIN_3, OUTPUT);
-  pinMode(Z_PIN_4, OUTPUT);
+  pinMode(Y_PIN_1, OUTPUT);
+  pinMode(Y_PIN_2, OUTPUT);
+  pinMode(Y_PIN_3, OUTPUT);
+  pinMode(Y_PIN_4, OUTPUT);
+
+  Serial.print("Initalized");
 
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
-// code for going back and forth using move
-/*
-  for(int i=1; i<=300; i++){
-    move_x(1);
-    move_z(1);
-  }
-  for(int i=1; i<=300; i++){
-    move_x(-1);
-    move_z(-1);
-    //set_angle((i/300)*180);
-  }
-
-  */
-
- // code for going back and forth using move_axes  
-  // move_axes(300, 300, 80);
-  // move_axes(-300, -300, 80);
-  // Serial.print(analogRead(A0));
-  // Serial.print("\n");
-
-  //code for testing if data received over serial
-  //retrieve data
-  listenToPi();
-  if(receivedData != ""){
-    Serial.print(receivedData);
-    receivedData = "";
-  }
-
   
+  if (DEBUG){ 
+    switch (MODE){
+      case 1 : //"move_back_and_forth"
+        //confirm that this section is being executed
+        Serial.print("Running move_back_and_forth \n");
+        // code for going back and forth using move_axes 
+        move_axes(200, 200, 90);
+        move_axes(-200, -200,180);
+        Serial.print(analogRead(A0));
+        Serial.print("\n");
+  
+        break;
+       
+      
+      case 2: //"commands_over_serial" 
+        //confirm that this section is being executed
+        Serial.print("Running commands_over_serial \n");
+        
+        //code for testing if data received over serial
+        // retrieve data
+        listenToPi();
+        
+        if(receivedData != ""){
+          Serial.print("Recieved Data: ");
+          Serial.print(receivedData);
+          Serial.print("\n");
+      
+          if (receivedData == "1"){
+            move_axes(-200, 0, 0);
+          }else if(receivedData == "2"){
+            move_axes(200, 0, 0);
+          }else if(receivedData == "3"){
+            move_axes(0, 200, 0);
+          }else if(receivedData == "4"){
+            move_axes(0, -200, 0);
+          }else if(receivedData == "5"){
+            move_axes(0, 0, 90);
+          }else if(receivedData == "6"){
+            move_axes(0, 0, 180);
+          }else{
+            move_axes(5, 5, 90);
+            move_axes(-5, -5,180);
+          }
+          //reset received data
+          receivedData = "";
+        }
+        
+        break;
+
+      case 3: //"back_and_forth_move_func"
+        //confirm that this section is being executed
+        Serial.print("Running back_and_forth_move_func \n");
+        // code for going back and forth using move
+        for(int i=1; i<=300; i++){
+          move_x(1);
+          move_y(1);
+        }
+        for(int i=1; i<=300; i++){
+          move_x(-1);
+          move_y(-1);
+          //set_angle((i/300)*180);
+        }
+        break;
+      
+      case 4: //"echo"
+        //confirm that this section is being executed
+        // Serial.print("Running echo \n");
+        //code for testing echo
+        listenToPi(); // retrieve data
+        
+        if(receivedData != ""){
+          Serial.print(receivedData);
+          // Serial.print("\n");
+          //parseSerialData(receivedData.c_str());
+          receivedData = "";
+        }else{
+          // do nothing
+        }
+        break;
+
+      case 5:// test for receiving and parsing g-code
+        
+        listenToPi(); // retrieve data
+        
+        if(receivedData != ""){
+          Serial.print(receivedData);
+          //Serial.print("\n");
+          //convert string to pointer and parse
+          //parseSerialData(receivedData);
+          
+          receivedData = "";
+        }else{
+          // do nothing
+        }
+        break;
+      
+      default:
+        Serial.print("Default case \n");
+
+    }
+  }else{
+    // do nothing
+  }
 }
